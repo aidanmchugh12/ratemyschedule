9e83583565f3199316e5d682219d8e21d24cdcbe
aidanmchugh12 <143359110+aidanmchugh12@users.noreply.github.com>
2024-09-29 04:05:55 -0400

    Styling

--- client/src/components/GradeOutput.jsx
+++ client/src/components/GradeOutput.jsx
@@ -69,6 +69,7 @@ class InputData extends React.Component {
       <head>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossOrigin="anonymous"></link>
       </head>
+      <h1>Your Report Card</h1>
       <div class="overall-grade-container">
         <h1>{overallGrade}</h1>
         <p>{overallGradeBlurb}</p>
